84579667fa3827c253f75e69598544c7182fa491
alex-santiago <alex.santiago.mail@gmail.com>
2017-11-01 23:28:41 -0700

    activity navigation

--- Battleship/app/src/main/java/com/hart/team/battleship/MainLogin.java
+++ Battleship/app/src/main/java/com/hart/team/battleship/MainLogin.java
@@ -1,13 +1,84 @@
 package com.hart.team.battleship;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainLogin extends AppCompatActivity {
+public class MainLogin extends AppCompatActivity
+    implements OnClickListener {
+
+
+    private EditText loginName;
+    private EditText password;
+    private Button btnLogin;
+    private Button btnSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        loginName = findViewById(R.id.editEmail);
+        password = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.buttonSignin);
+        btnSignup = findViewById(R.id.buttonSignUp);
+
+        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSignin:
+                signInUser();
+                break;
+            case R.id.buttonSignUp:
+                signUpUser();
+                break;
+        }
+    }
+
+    private Boolean validateSignIn(){
+        if (loginName.getText().toString().isEmpty()) {
+            Toast.makeText(this,
+                    "You must inform the user login.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.getText().toString().isEmpty()) {
+            Toast.makeText(this,
+                    "You must inform the user password.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void signInUser() {
+        if (validateSignIn()) {
+            Toast.makeText(this,
+                    "User signed-in",
+                    Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent("com.hart.team.battleship.Board");
+            startActivity(intent);
+        }
+    }
+
+    private void signUpUser() {
+        Toast.makeText(this,
+                "Sign-up user",
+                Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent("com.hart.team.battleship.SignUp");
+        startActivity(intent);
     }
 }
--- Battleship/app/src/main/java/com/hart/team/battleship/SignUp.java
+++ Battleship/app/src/main/java/com/hart/team/battleship/SignUp.java
@@ -2,12 +2,90 @@ package com.hart.team.battleship;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity
+    implements View.OnClickListener {
+
+    private EditText loginEmail;
+    private EditText password;
+    private EditText passwordConfirm;
+    private Button btnOkay;
+    private Button btnCancel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        loginEmail = findViewById(R.id.editEmail);
+        password = findViewById(R.id.editPassword);
+        passwordConfirm = findViewById(R.id.editConfirmPassword);
+        btnOkay = findViewById(R.id.buttonOkay);
+        btnCancel = findViewById(R.id.buttonCancel);
+
+        btnOkay.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonOkay:
+                if (validateSignUp()) {
+                    signUpUser();
+                    returnToSignIn();
+                }
+                break;
+            case R.id.buttonCancel:
+                returnToSignIn();
+                break;
+        }
+    }
+
+    private void returnToSignIn(){
+        finish();
+    }
+
+    private Boolean validateSignUp(){
+        if (loginEmail.getText().toString().isEmpty()) {
+            Toast.makeText(this,
+                    "You must inform the e-mail for user login.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (this.password.getText().toString().isEmpty()) {
+            Toast.makeText(this,
+                    "You must inform the user password.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (this.passwordConfirm.getText().toString().isEmpty()) {
+            Toast.makeText(this,
+                    "You must confirm the user password.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!(this.password.getText().toString().equals(this.passwordConfirm.getText().toString()))) {
+            Toast.makeText(this,
+                    "The password confirmation does not match the informed password.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        Toast.makeText(this,
+                "User signed-up.",
+                Toast.LENGTH_SHORT).show();
+        return true;
+
+    }
+
+    private void signUpUser(){
+
+    }
+
 }
